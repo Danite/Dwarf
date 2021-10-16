62b14e8b1df488caf5348f4a335bbb414baf3c3c
danite <luansnain@gmail.com>
2021-10-16 23:09:39 +0200

    Add a stub resolver for simulating an DNS client

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,53 @@
 use std::error::Error;
-use std::fs::File;
-use std::io::Read;
+use std::net::UdpSocket;
 
 pub use self::components::*;
+pub use self::types::*;
 
 mod components;
 mod types;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut response_packet_file = File::open("./packets/response_packet.txt")?;
-    let mut buffer = BytePacketBuffer::new();
+    let qname = "google.com";
+    let qtype = QueryType::A;
 
-    response_packet_file.read(&mut buffer.buffer)?;
+    let server = ("8.8.8.8", 53);
 
-    let packet = DnsPacket::from_buffer(&mut buffer)?;
-    println!("{:#?}", packet.header);
+    let socket = UdpSocket::bind(("0.0.0.0", 43210))?; // arbitrary port
 
-    for question in packet.questions {
+    let mut packet = DnsPacket::new();
+
+    packet.header.id = 6666;
+    packet.header.questions = 1;
+    packet.header.recursion_desired = true;
+    packet
+        .questions
+        .push(DnsQuestion::new(qname.to_string(), qtype));
+
+    let mut req_buffer = BytePacketBuffer::new();
+    packet.write(&mut req_buffer)?;
+
+    socket.send_to(&req_buffer.buffer[0..req_buffer.position], server)?;
+
+    let mut res_buffer = BytePacketBuffer::new();
+    socket.recv_from(&mut res_buffer.buffer)?;
+
+    let res_packet = DnsPacket::from_buffer(&mut res_buffer)?;
+    println!("{:#?}", res_packet);
+
+    for question in res_packet.questions {
         println!("{:#?}", question);
     }
 
-    for answer_record in packet.answers {
+    for answer_record in res_packet.answers {
         println!("{:#?}", answer_record);
     }
 
-    for authority_record in packet.authorities {
+    for authority_record in res_packet.authorities {
         println!("{:#?}", authority_record);
     }
 
-    for resource_record in packet.resources {
+    for resource_record in res_packet.resources {
         println!("{:#?}", resource_record);
     }
 
